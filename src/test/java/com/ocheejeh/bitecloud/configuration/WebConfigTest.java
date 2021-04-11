22cd91e32c2ab3ddceee6e55b3421fc1699761e7
apple <apple@apples-MacBook-Pro.local>
2021-04-11 15:18:34 +0100

    add app config directory

---
+++ src/test/java/com/ocheejeh/bitecloud/configuration/WebConfigTest.java
@@ -0,0 +1,26 @@
+package com.ocheejeh.bitecloud.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest
+class WebConfigTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldLoadHomeView() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to Bite Cloud")));
+    }
+}
